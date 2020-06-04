fe398f43e4c8f7f48dfd206b346caa94721f2a28
DMA-47 <44584538+DMA-47@users.noreply.github.com>
2020-06-05 02:09:31 +0300

    Added Unit Of Work

---
+++ Catalog/DAL/DAL/EF/EFUnitOfWork.cs
@@ -0,0 +1,66 @@
+﻿using Catalog.DAL.Entities;
+using Catalog.DAL.Repositories.Impl;
+using Catalog.DAL.Repositories.Interfaces;
+using Catalog.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Catalog.DAL.EF
+{
+    public class EFUnitOfWork
+        : IUnitOfWork
+    {
+        private ProjectContext db;
+        private ProductRepository productRepository;
+        private StorageRepository storageRepository;
+
+        public EFUnitOfWork(ProjectContext context)
+        {
+            db = context;
+        }
+        public IProductRepository Products
+        {
+            get
+            {
+                if (productRepository == null)
+                    productRepository = new ProductRepository(db);
+                return productRepository;
+            }
+        }
+
+        public IStorageRepository Storages
+        {
+            get
+            {
+                if (storageRepository == null)
+                    storageRepository = new StorageRepository(db);
+                return storageRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
