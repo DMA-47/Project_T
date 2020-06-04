4145e0beb079a562d086112e28ed7a15fb656572
DMA-47 <44584538+DMA-47@users.noreply.github.com>
2020-06-05 01:59:41 +0300

    Added Impl

---
+++ Catalog/DAL/DAL/EF/ProjectContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.DAL.EF
+{
+    public class ProjectContext
+        : DbContext
+    {
+        public DbSet<Product> Osbbs { get; set; }
+        public DbSet<Storage> Streets { get; set; }
+
+        public ProjectContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+    }
+}
