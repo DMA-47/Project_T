d57d57590193c456010a55d5ce885fa71790ccf6
DMA-47 <44584538+DMA-47@users.noreply.github.com>
2020-06-05 01:10:43 +0300

    Added Entities to DAL

---
+++ Catalog/DAL/DAL/Entities/Storage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.DAL.Entities
+{
+	class Storage
+	{
+		public int Id { get; set; }
+		public Product List_product { get; set; }
+		public int Num_list { get; set; }
+	}
+}
