5d6ee933100dd682de4ab996758350a02d5c3aee
DMA-47 <44584538+DMA-47@users.noreply.github.com>
2020-06-06 00:14:37 +0300

    Added Identity

---
+++ Project/CCL/Security/Identity/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Security.Identity
+{
+    public class Admin
+        : User
+    {
+        public Admin(int userId, string name, int osbbId)
+            : base(userId, name, nameof(Admin))
+        {
+        }
+    }
+}
---
+++ Project/CCL/Security/Identity/StorageManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCL.Security.Identity
+{
+    public class StorageManager
+        : User
+    {
+        public StorageManager(int userId, string name, int osbbId)
+            : base(userId, name, nameof(StorageManager))
+        {
+        }
+    }
+}
