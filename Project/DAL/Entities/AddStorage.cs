ad7c9ce6ee7c817ccc367e5f33f860aa92f2abf1
DMA-47 <44584538+DMA-47@users.noreply.github.com>
2020-06-06 21:46:33 +0300

    Lab 12

---
+++ Project/DAL/Entities/AddStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.DAL.Entities
+{
+	class AddStorage : Storage
+	{
+		public int AddId { get; set; }
+		public override void ShowAddress()
+		{
+			Console.WriteLine("Additional storage, id main storage:", AddId);
+		}
+	}
+}
--- Project/DAL/Entities/Storage.cs
+++ Project/DAL/Entities/Storage.cs
@@ -7,9 +7,25 @@ namespace Catalog.DAL.Entities
 	public class Storage
 	{
 		public int Id { get; set; }
+		public string address { get; set; }
 		public Product List_product { get; set; }
 		public int Num_list { get; set; }
 
 		public void metod2() { }
+
+		public void AllShow()
+		{
+			ShowId();
+			ShowAddress();
+		}
+		public virtual void ShowId()
+		{
+			Console.WriteLine(Id);
+		}
+		public virtual void ShowAddress()
+		{
+			Console.WriteLine(address);
+		}
+		
 	}
 }
